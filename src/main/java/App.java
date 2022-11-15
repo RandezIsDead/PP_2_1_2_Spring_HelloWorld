65b28fa70c69acbfb6d299bdeaa9dfec7e1a85fc
RandezIsDead <hckrden@gmail.com>
2022-11-16 00:01:28 +1000

    POJO bean Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,15 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld h1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld h2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat c1 = (Cat) applicationContext.getBean("cat");
+        Cat c2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(h1 == h2);
+        System.out.println(c1 == c2);
     }
 }
